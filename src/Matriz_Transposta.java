28f6949395cd23db9bfcac766be89a7e4a43530b
GabrielGonsalves201104 <gabriel201104@edu.unipar.br>
2025-09-18 20:54:14 -0300

    feat: Adiciona a lógica para calcular e exibir a transposta de uma matriz.

---
+++ src/Matriz_Transposta.java
@@ -0,0 +1,36 @@
+public class Matriz_Transposta {
+    public static void main(String[] args) {
+        int[][] matrizOriginal = {
+                {1, 2},
+                {3, 4},
+                {5, 6}
+        };
+
+        int linhasOriginal = matrizOriginal.length;
+        int colunasOriginal = matrizOriginal[0].length;
+
+        int[][] matrizTransposta = new int[colunasOriginal][linhasOriginal];
+
+        for (int i = 0; i < linhasOriginal; i++) {
+            for (int j = 0; j < colunasOriginal; j++) {
+                matrizTransposta[j][i] = matrizOriginal[i][j];
+            }
+        }
+
+        System.out.println("Matriz Original (3x2):");
+        for (int i = 0; i < linhasOriginal; i++) {
+            for (int j = 0; j < colunasOriginal; j++) {
+                System.out.print(matrizOriginal[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nMatriz Transposta (2x3):");
+        for (int i = 0; i < colunasOriginal; i++) {
+            for (int j = 0; j < linhasOriginal; j++) {
+                System.out.print(matrizTransposta[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
