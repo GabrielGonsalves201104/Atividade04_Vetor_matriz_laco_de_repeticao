5898c96e9c8b3dec085dbc3405f3f5691341cd0c
GabrielGonsalves201104 <gabriel201104@edu.unipar.br>
2025-09-18 20:40:30 -0300

    feat: Implementa a funcionalidade para encontrar o maior e o menor número em um vetor.

---
+++ src/Maior_menor.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Maior_menor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[10];
+
+        System.out.println("Por favor, digite 10 números inteiros:");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = sc.nextInt();
+        }
+
+        int maior = numeros[0];
+        int menor = numeros[0];
+
+        for (int i = 1; i < 10; i++) {
+            if (numeros[i] > maior) {
+                maior = numeros[i];
+            }
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+        }
+
+        System.out.println("O maior número digitado foi: " + maior);
+        System.out.println("O menor número digitado foi: " + menor);
+        sc.close();
+    }
+}
